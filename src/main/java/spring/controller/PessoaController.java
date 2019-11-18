f3d03dca0344cc6b7a1a905693e2f96a9fd3446c
Jefferson Brandao <jeffersonbraster@gmail.com>
2019-11-17 21:35:21 -0300

    carregando lista de pessoas cadastrados e fazendo melhorias

--- src/main/java/spring/controller/PessoaController.java
+++ src/main/java/spring/controller/PessoaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import spring.entidade.Pessoa;
 import spring.repository.PessoaRepository;
@@ -20,9 +21,23 @@ public class PessoaController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
-	public String salvar(Pessoa pessoa) {
+	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
-		return "cadastro/cadastropessoa";
+		
+		ModelAndView andVier = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andVier.addObject("pessoas", pessoasIt);
+		
+		
+		return andVier;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value="/listapessoas")
+	public ModelAndView pessoas() {
+		ModelAndView andVier = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andVier.addObject("pessoas", pessoasIt);
+		return andVier;
 	}
 	
 
