3621786fab4a081a9a0e08716cf244ac19cfbf84
Jefferson Brandao <jeffersonbraster@gmail.com>
2019-11-21 20:19:46 -0300

    adicionando a tela de detalhes das pessoas

--- src/main/java/spring/controller/PessoaController.java
+++ src/main/java/spring/controller/PessoaController.java
@@ -29,6 +29,11 @@ public class PessoaController {
 				
 		//retorna um objeto vazio, para evitar erro
 		modelAndView.addObject("pessoaobj", new Pessoa());
+		
+		//carrega lista de pessoas ao abrir a pagina
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		modelAndView.addObject("pessoas", pessoasIt);
+		
 		return modelAndView;
 	}
 	
@@ -94,6 +99,20 @@ public class PessoaController {
 		
 		return modelAndView;
 		
+	}
+	
+	@GetMapping("/telefones/{idpessoa}") //mapeou a url criada no html
+	public ModelAndView telefones(@PathVariable("idpessoa") Long idpessoa) {
+		//carrega/consulta o objeto do banco de dados
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+		
+		//prepara o retorno do modo envio para a mesma tela
+		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
+		
+		//prepara o objeto para edição
+		modelAndView.addObject("pessoaobj", pessoa.get());
+		
+		return modelAndView;
 	}
 	
 
