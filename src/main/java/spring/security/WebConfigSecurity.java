5fc2a3fada7eaebc54f6a06acb1512fffcf9072e
Jefferson Brandao <jeffersonbraster@gmail.com>
2019-11-24 21:54:04 -0300

    configurando spring security em memoria

---
+++ src/main/java/spring/security/WebConfigSecurity.java
@@ -0,0 +1,46 @@
+package spring.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
+	
+	@Override //configura as solicitações de acesso por http
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf()
+		.disable() //desativa as configurações padrão de memória
+		.authorizeRequests() //permitir restringir acessos
+		.antMatchers(HttpMethod.GET, "/").permitAll() //qualquer usuario acessa
+		.anyRequest().authenticated()
+		.and().formLogin().permitAll() //permite qualquer usuário
+		.and().logout() //mapeia url de logout do sistema e invalida usuario autenticado
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}
+	
+	@Override //cria autenticação do usuario com banco de dados ou em memoria
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.inMemoryAuthentication()
+			.passwordEncoder(NoOpPasswordEncoder.getInstance())	
+			.withUser("jeje")
+			.password("jeje")
+			.roles("ADMIN");
+			
+	}
+	
+	@Override //ignora url especificas
+	public void configure(WebSecurity web) throws Exception {
+		
+		web.ignoring().antMatchers("/materialize/**");
+	}
+
+}
