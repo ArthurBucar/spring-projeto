a5e97e4ec95685f78e2b489aa7763c2fea96159b
Jefferson Brandao <jeffersonbraster@gmail.com>
2019-11-30 10:42:07 -0300

    implementando o viacep no formulario

--- src/main/java/spring/entidade/Pessoa.java
+++ src/main/java/spring/entidade/Pessoa.java
@@ -33,6 +33,12 @@ public class Pessoa implements Serializable {
 	@Min(value = 18, message = "Idade minima 18 anos")
 	private int idade;
 	
+	private String cep;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String uf;
+	
 	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL)
 	private List<Telefone> telefones;
 	
@@ -80,6 +86,48 @@ public class Pessoa implements Serializable {
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+	
+	
 	
 	
 	
