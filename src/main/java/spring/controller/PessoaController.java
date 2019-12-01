23d61159fed6eefbb9f94dede2273f5d980a404d
Jefferson Brandao <jeffersonbraster@gmail.com>
2019-12-01 11:21:25 -0300

    criacao do metodo de impressao no controller

--- src/main/java/spring/controller/PessoaController.java
+++ src/main/java/spring/controller/PessoaController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,9 @@ public class PessoaController {
 	
 	@Autowired
 	private TelefoneRepository telefoneRepository;
+	
+	@Autowired
+	private ReportUtil reportUtil;
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
 	public ModelAndView inicio() {
@@ -214,6 +218,15 @@ public class PessoaController {
 		modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoa.getId()));
 		
 		return modelAndView;
+	}
+	
+	@GetMapping("**/pesquisarpessoa")
+	public void imprimePdf(@RequestParam("nomepesquisa") String nomepesquisa,
+			@RequestParam("pesqsexo") String pesqsexo,
+			HttpServletRequest request, HttpServletRequest response) {
+		
+		System.out.println("sauhsahusahuashu");
+		
 	}
 	
 
